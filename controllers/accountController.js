445a8dd6ca90ecc750c865e62a7e7fb359f969ea
Margarita Lemkova <margaretles675@gmail.com>
2024-04-24 21:38:38 +0400

    basic auth

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,13 @@
 const Account = require('../models/account')
+const bcrypt = require('bcrypt')
+const ApiError = require('./apiError')
+const jwt = require('jsonwebtoken')
 
+const generateJwt = (id, email, role) => {
+    return jwt.sign({id, email, role},
+        process.env.ACCESS_SECRET_KEY,
+        {expiresIn: '15m'})
+}
 
 class AccountController {
 
@@ -7,12 +15,35 @@ class AccountController {
     async createAccount(req, res) {
 
         let {surname, name, patronymic, email, password, roleId} = req.body
-        const account = await Account.create({surname, name, patronymic, email, password, roleId})
+        const hashPassword = await bcrypt.hash(password, 5)
+        const account = await Account.create({surname, name, patronymic, email, password: hashPassword, roleId})
         return res.json(account)
 
     }
 
-    async getAll(req, res) {
+    async login(req, res, next) {
+
+        let {email, password} = req.body
+        const user = await Account.findOne({where: {email}})
+        if (!user) {
+            return next(ApiError.internalError('Пользователь не найден'))
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.internalError('Неверный пароль'))
+        }
+        const accessToken = generateJwt(user.id, user.email, user.roleId)
+        const refresh_token =  jwt.sign((user.id, user.email, user.roleId), process.env.REFRESH_SECRET_KEY)
+        return res.json({accessToken})
+
+    }
+
+    async getInformation(req, res) {
+
+        const {id} = req.params
+        const user = await Account.findOne({where: {id}})
+
+        return res.json(user)
         
     }
 }
--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -1,10 +1,11 @@
 const Router = require('express')
 const router = new Router()
 const accountController = require('../controllers/accountController')
+const authMiddleware = require('../middleware/authMiddleware')
 
-router.post('/', accountController.createAccount)
-router.get('/', (req, res) => {
-    res.json({message: 'uuuuu'})
-})
+router.post('/create', accountController.createAccount)
+router.post('/login', accountController.login)
+router.get('/:id', authMiddleware, accountController.getInformation)
+
 
 module.exports = router
